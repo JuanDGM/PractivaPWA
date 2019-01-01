06365032e47b2f48289f5e05334e54d5d028a9eb
JuanDGM <juan84_10@hotmail.com>
2018-12-31 19:34:18 -0500

    decimos

--- sw-utils.js
+++ sw-utils.js
@@ -1,19 +1,13 @@
 
 
 function actualizaCacheDinamico(dynamicCache, req, res){
-    
     if(res.ok){
-        
         return caches.open(dynamicCache).then(cache=>{
-            
             cache.put(req, res);
             return res;
-            
         });
     }else{
-        
         return res;
-        
     }
 }
 
