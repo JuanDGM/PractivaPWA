6de82da29d4df261d3eb38b520be253cb43ff328
JuanDGM <juan84_10@hotmail.com>
2018-12-31 20:18:59 -0500

    catorce

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 
 if(navigator.serviceWorker){
-   navigator.serviceWorker.register('sw.js'); 
+   //navigator.serviceWorker.register('sw.js'); 
+   navigator.serviceWorker.register('/PractivaPWA/sw.js'); 
 }
