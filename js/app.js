1a792ce851fe89a264c675aba2f2a99ba08a1f61
JuanDGM <juan84_10@hotmail.com>
2019-01-01 20:51:58 -0500

    21

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 
+//var url =window.location.href;
+
+//var 
+
 
 if(navigator.serviceWorker){
    //navigator.serviceWorker.register('sw.js'); 
-   navigator.serviceWorker.register('/PractivaPWA/sw.js'); 
+   navigator.serviceWorker.register('/JuanDGM/PractivaPWA/sw.js'); 
 }
